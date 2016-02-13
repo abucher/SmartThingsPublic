228b5fdf85213584020a1e6a0739cd55c3b7a9b3
Aaron Bucher <aaron@abucher.com>
2016-02-12 18:01:24 -0600

    Added yamahaDiscovery function.

--- smartapps/abucher/yamaha-av-controller.src/yamaha-av-controller.groovy
+++ smartapps/abucher/yamaha-av-controller.src/yamaha-av-controller.groovy
@@ -28,6 +28,52 @@ preferences {
 	page(name:"yamahaDiscovery", title:"Yamaha Device Setup", content:"yamahaDiscovery", refreshTimeout:5)
 }
 
+/**
+ * Device discovery
+ */
+def yamahaDiscovery () {
+	if(canInstallLabs()) {
+		int refreshCount = !state.refreshCount ? 0 : state.refreshCount as int
+		state.refreshCount += 1
+		def refreshInterval = 3
+
+		def options = yamahasDiscovered() ?: []
+
+		def numFound = options.size() ?: 0
+
+		if(!state.subscribe) {
+			log.trace "subscribe to location"
+			subscribe(location, null, locationHandler, [filterEvents:false])
+			state.subscribe = true
+		}
+
+		// Yamaha discovery request every 5 //25 seconds
+		if((refreshCount % 8) == 0) {
+			discoverYamahas()
+		}
+
+		// GetBasicStatus request every 3 seconds except on discoveries
+		if(((refreshCount % 1) == 0) && ((refreshCount % 8) != 0)) {
+			verifyYamahaDevice()
+		}
+
+		return dynamicPage(name:"yamahaDiscovery", title:"Yamaha Discovery Started!", nextPage:"", refreshInterval:refreshInterval, install:true, uninstall: true) {
+			section("Please wait while we discover your Yamaha device... discovery can take five minutes or more, so sit back and relax! Select your device below once discovered.") {
+				input "selectedYamaha", "enum", required:false, title:"Select Yamaha (${numFound} found)", multiple:true, options:options
+			}
+		}
+	}
+	else {
+		return dynamicPage(name:"yamahaDiscovery", title:"Upgrade needed!", nextPage:"", install:false, uninstall: true) {
+			section("Upgrade") {
+				paragraph """To use SmartThings Labs, your Hub should be completely up to date.
+
+To update your Hub, access Location Settings in the Main Menu (tap the gear next to your location name), select your Hub, and choose "Update Hub"."""
+			}
+		}
+	}
+}
+
 def installed() {
 	log.debug "Installed with settings: ${settings}"
 
